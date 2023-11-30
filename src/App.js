fccbe9ad9d0514e93728a595e01e43be4f4d04ea
eXodd <santosbjorge@gmail.com>
2023-11-30 02:02:04 -0600

    Update dependencies and add simple gui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleUrlChange = (event) => {
+    setImageUrl(event.target.value);
+  };
+
+  const handleAnalysisClick = () => {
+    // Perform image analysis logic here
+    console.log('Image analysis triggered');
+  };
+
+  const handleGenerationClick = () => {
+    // Perform image generation logic here
+    console.log('Image generation triggered');
+  };
+
+  return (
+    <div>
+      <title>Computer Vision</title>
+      <div>
+        <h1>Computer Vision</h1>
+        <h2>Image Analysis</h2>
+        <input type="text" value={imageUrl} onChange={handleUrlChange} placeholder="Enter image URL, or type prompt" />
+        <div>
+          <button onClick={handleAnalysisClick}>Analyze Image</button>
+          <button onClick={handleGenerationClick}>Generate Image</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
